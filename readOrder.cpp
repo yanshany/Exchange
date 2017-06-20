3296b1a4532e2856a44445024c5c53275bb15ae6
yanshan <yanshan@aecapital.com.au>
2017-06-20 12:20:39 +1000

    prority on aggressive order not implemented yet

--- readOrder.cpp
+++ readOrder.cpp
@@ -13,61 +13,72 @@ int main () {
 string s;
 string delimiter = ":";
 
-vector<Order> myOrder;
+vector<Order> allOrder;
 int i = 0;
 while(getline(cin,s)){
 //	cout << s << endl;
-	myOrder.push_back(parseLineToOrder(s, delimiter));
-	myOrder[i].print();
+	allOrder.push_back(parseLineToOrder(s, delimiter));
+	allOrder[i].print();
 	i++;
 }
 
+Order newOrder = Order();
 vector<Trade> myTrade;
-vector<Order> openOrder;
-openOrder.push_back(myOrder[0]);
+vector<Order> openOrder,openOrderCopy;
+openOrder.push_back(allOrder[0]);
+openOrderCopy = openOrder;
 
-for (int i = 1; i != myOrder.size(); ++i ){
-	for (int j = 0; j != openOrder.size(); ++j) {
-		if(myOrder[i].instrument == openOrder[j].instrument && myOrder[i].quantity*openOrder[j].quantity < 0)
+for (int i = 1; i != allOrder.size(); ++i ){
+	newOrder = allOrder[i];
+	for (int j = 0; j != openOrderCopy.size(); ++j) {
+
+		if(newOrder.instrument == openOrder[j].instrument && newOrder.quantity*openOrder[j].quantity < 0)
 		{
-			if(myOrder[i].quantity > 0 && myOrder[i].price > openOrder[j].price)
+			if(newOrder.quantity > 0 && newOrder.price >= openOrder[j].price)
 			{
-				myTrade.push_back(Trade(myOrder[i].name, openOrder[j].name, myOrder[i].instrument,min(myOrder[i].quantity,-1*openOrder[j].quantity), openOrder[j].price));				
-				if(myOrder[i].quantity < -1*openOrder[j].quantity) openOrder[j].quantity += myOrder[i].quantity;
-				if(myOrder[i].quantity == -1*openOrder[j].quantity) openOrder.erase(openOrder.begin()+j);
-				if(myOrder[i].quantity > -1*openOrder[j].quantity) 
+				myTrade.push_back(Trade(newOrder.name, openOrder[j].name, newOrder.instrument,min(newOrder.quantity,-1*openOrder[j].quantity), openOrder[j].price));				
+				if(newOrder.quantity < -1*openOrder[j].quantity)
+					 openOrder[j].quantity += newOrder.quantity;
+				else if (newOrder.quantity > -1*openOrder[j].quantity) 
 				{
+					openOrder.push_back(Order(newOrder.name,newOrder.instrument,newOrder.quantity + openOrder[j].quantity, newOrder.price));
 					openOrder.erase(openOrder.begin()+j);
-					openOrder.push_back(Order(myOrder[i].name,myOrder[i].instrument,myOrder[i].quantity + openOrder[j].quantity, myOrder[i].price));
 				}
+				else 
+					openOrder.erase(openOrder.begin()+j);
+				
 
 			}				
-			else if (myOrder[i].quantity < 0 && myOrder[i].price < openOrder[j].price)
+			else if (newOrder.quantity < 0 && newOrder.price <= openOrder[j].price)
 			{
-				myTrade.push_back(Trade(openOrder[j].name, myOrder[i].name, myOrder[i].instrument,min(-1*myOrder[i].quantity,openOrder[j].quantity), openOrder[j].price));				
-				if(-1*myOrder[i].quantity < openOrder[j].quantity) openOrder[j].quantity += myOrder[i].quantity;
-				if(-1*myOrder[i].quantity == openOrder[j].quantity) openOrder.erase(openOrder.begin()+j);
-				if(-1*myOrder[i].quantity > openOrder[j].quantity) 
+				myTrade.push_back(Trade(openOrder[j].name, newOrder.name, newOrder.instrument,min(-1*newOrder.quantity,openOrder[j].quantity), openOrder[j].price));				
+				if(-1*newOrder.quantity < openOrder[j].quantity) 
+					openOrder[j].quantity += newOrder.quantity;
+				else if(-1*newOrder.quantity > openOrder[j].quantity) 
 				{
+					openOrder.push_back(Order(newOrder.name,newOrder.instrument,newOrder.quantity + openOrder[j].quantity, newOrder.price));
 					openOrder.erase(openOrder.begin()+j);
-					openOrder.push_back(Order(myOrder[i].name,myOrder[i].instrument,myOrder[i].quantity + openOrder[j].quantity, myOrder[i].price));
 				}
+				else
+					openOrder.erase(openOrder.begin()+j);
+
 			}
 			else
-				openOrder.push_back(myOrder[i]);
+				openOrder.push_back(newOrder);
 					
 		}
 		else
-			openOrder.push_back(myOrder[i]);
+			openOrder.push_back(newOrder);
 
 	}
+	openOrderCopy = openOrder;
 }
 
-
+cout << "trade:\n";
 for (int i = 0; i != myTrade.size() ; ++i){
 	myTrade[i].print();
 }
-
+cout << "open order:\n";
 for (int i = 0; i != openOrder.size() ; ++i){
 	openOrder[i].print();
 }
