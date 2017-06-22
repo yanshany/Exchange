a4e318fdff308fbf78ccbce81bed4d701390fc22
yanshan <yanshan@aecapital.com.au>
2017-06-22 18:36:59 +1000

    get correct result

--- main.cpp
+++ main.cpp
@@ -8,111 +8,127 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <memory>
 using namespace std;
 
-int main () {
+int main()
+{
 
-int indBestCandidate;
-string s;
-string delimiter = ":";
-Order newOrder = Order();
-vector<Order> allOrder;
-vector<Trade> myTrade;
-vector<int> ind;
-vector<Order> openOrder, openOrderCopy, candidate;
+	string s;
+	string delimiter = ":";
+	Order newOrder = Order();
+	vector<Order> allOrder;
+	vector<Trade> myTrade;
+    vector<shared_ptr<Order>> openOrder;
+    vector<shared_ptr<Order>> candidate;
 
-
-// read in orders from standard input
-while(getline(cin,s)){
-	allOrder.push_back(parseLineToOrder(s, delimiter));
-	allOrder.back().print();
-}
-
-openOrder.push_back(allOrder[0]);
-openOrderCopy = openOrder;
-
-for (int i = 1; i != allOrder.size(); ++i ){
-	newOrder = allOrder[i];
-	if (newOrder.quantity > 0) // buy order
+	// read in orders from standard input
+	while (getline(cin, s))
 	{
-		// find sell orders in order book matching instument and acceptable price
-		// the order book copy is used in iteration of for loop. Because we change the size of openOrder in the for loop, openOrder can not be usd in the iteration of for loop
-		for (int j = 0; j != openOrderCopy.size(); ++j){
-			if(newOrder.instrument == openOrder[j].instrument && openOrder[j].quantity < 0 && openOrder[j].price <= newOrder.price)
-			{
-				ind.push_back(j);
-				candidate.push_back(openOrder[j]);
-			}								
-		}
-
-		// cannot find matching order, add the new order in order book
-		if (ind.size() == 0)
-			openOrder.push_back(newOrder);
-
-		// find one mathching order, update the order book
-		if (ind.size() == 1)
-		{
-			myTrade.push_back(Trade(newOrder.name, candidate[0].name, newOrder.instrument,min(newOrder.quantity,-1*candidate[0].quantity), candidate[0].price));				
-			openOrder = updateOpenOrder(newOrder,openOrder,ind[0]);	
-		}
-
-		// find more than one matching order, choose the best one which is the first lowest price, update the order book
-		if (ind.size() > 1)
-		{
-			indBestCandidate = chooseCandidate(newOrder,candidate);
-			myTrade.push_back(Trade(newOrder.name, openOrder[ind[indBestCandidate]].name, newOrder.instrument,min(newOrder.quantity,-1*openOrder[ind[indBestCandidate]].quantity), openOrder[ind[indBestCandidate]].price));				
-			openOrder = updateOpenOrder(newOrder,openOrder,ind[indBestCandidate]);
-		}
-		
-		// clear variables
-		ind.clear(); 
-		candidate.clear();
+		allOrder.push_back(parseLineToOrder(s, delimiter));
 	}
-	if (newOrder.quantity < 0) // sell order
+
+	openOrder.push_back(make_shared<Order>(allOrder[0]));
+
+	for (int i = 1; i != allOrder.size(); ++i)
 	{
-		// find sell orders in order book matching instument and acceptable price
-		for (int j = 0; j != openOrderCopy.size(); ++j){
-			if(newOrder.instrument == openOrder[j].instrument && openOrder[j].quantity > 0 && openOrder[j].price >= newOrder.price)
+		newOrder = allOrder[i];
+		if (newOrder.quantity > 0) // buy order
+		{
+			// find sell orders in order book matching instument and acceptable price
+			// the order book copy is used in iteration of for loop. Because we change the size of openOrder in the for loop, openOrder can not be usd in the iteration of for loop
+			for (int j = 0; j != openOrder.size(); ++j)
 			{
-				ind.push_back(j);
-				candidate.push_back(openOrder[j]);
-			}								
+				if (newOrder.instrument == openOrder[j]->instrument && openOrder[j]->quantity < 0 && openOrder[j]->price <= newOrder.price)
+				{
+					candidate.push_back(openOrder[j]);
+				}
+			}
+
+			// cannot find matching order, add the new order in order book
+			if (candidate.size() == 0)
+				openOrder.push_back(make_shared<Order>(newOrder));
+			else
+			{
+				// sort candidate according to price
+                sort(candidate.begin(),candidate.end(),[](const shared_ptr<Order>& lhs, const shared_ptr<Order>& rhs)
+                {
+                        return lhs->price < rhs->price;
+                });
+				// matching new order with candidate in order book
+				for (int k = 0; k != candidate.size(); ++k)
+				{
+					myTrade.push_back(Trade(newOrder.name, candidate[k]->name, newOrder.instrument, min(newOrder.quantity, -1 * candidate[k]->quantity), candidate[k]->price));
+					if (newOrder.quantity < -1 * candidate[k]->quantity)
+					{
+						candidate[k]->quantity += newOrder.quantity;
+						break;
+					}
+					else if (newOrder.quantity > -1 * candidate[k]->quantity)
+					{
+						newOrder.quantity += candidate[k]->quantity;
+						candidate[k]->quantity = 0;
+					}
+					else
+					{
+						candidate[k]->quantity = 0;
+						break;
+					}	
+				}
+			}
+			// clear variables
+			candidate.clear();
 		}
-
-		// cannot find matching order, add the new order in order book
-		if (ind.size() == 0 )
-			openOrder.push_back(newOrder); 
-
-		// find one mathching order, update the order book
-		if (ind.size() == 1)
+		if (newOrder.quantity < 0) // sell order
 		{
-			myTrade.push_back(Trade(openOrder[ind[0]].name,newOrder.name, newOrder.instrument,min(-1*newOrder.quantity,openOrder[ind[0]].quantity), openOrder[ind[0]].price));				
-			openOrder = updateOpenOrder(newOrder,openOrder,ind[0]);		
-		}
+			// find sell orders in order book matching instument and acceptable price
+			for (int j = 0; j != openOrder.size(); ++j)
+			{
+				if (newOrder.instrument == openOrder[j]->instrument && openOrder[j]->quantity > 0 && openOrder[j]->price >= newOrder.price)
+				{
+					candidate.push_back(openOrder[j]);
+				}
+			}
 
-		// find more than one matching order, choose the best one which is the first lowest price, update the order book
-		if (ind.size() > 1)
-		{
-			indBestCandidate = chooseCandidate(newOrder,candidate);
-			myTrade.push_back(Trade(openOrder[ind[indBestCandidate]].name, newOrder.name, newOrder.instrument,min(-1*newOrder.quantity,openOrder[ind[indBestCandidate]].quantity), openOrder[ind[indBestCandidate]].price));				
-			openOrder = updateOpenOrder(newOrder,openOrder,ind[indBestCandidate]);
+			// cannot find matching order, add the new order in order book
+			if (candidate.size() == 0)
+				openOrder.push_back(make_shared<Order>(newOrder));
+			else
+			{
+				// sort candidate according to price
+                sort(candidate.begin(),candidate.end(),[](const shared_ptr<Order>& lhs, const shared_ptr<Order>& rhs)
+                {
+                        return lhs->price > rhs->price;
+                });
+				// matching new order with candidate in order book
+				for (int k = 0; k != candidate.size(); ++k)
+				{
+					myTrade.push_back(Trade(candidate[k]->name, newOrder.name,newOrder.instrument, min(-1*newOrder.quantity, candidate[k]->quantity), candidate[k]->price));
+					if (-1*newOrder.quantity < candidate[k]->quantity)
+					{
+						candidate[k]->quantity += newOrder.quantity;
+						break;
+					}	
+					else if (-1*newOrder.quantity > candidate[k]->quantity)
+					{
+						newOrder.quantity += candidate[k]->quantity;
+						candidate[k]->quantity = 0;
+					}
+					else
+					{
+						candidate[k]->quantity = 0;
+						break;
+					}	
+				}
+			}
+			// clear variables
+			candidate.clear();
 		}
-
-		// clear variables
-		ind.clear(); 
-		candidate.clear();
 	}
-	// update the order book copy
-	openOrderCopy = openOrder;
-}
 
-cout << "trade:\n";
-for (int i = 0; i != myTrade.size() ; ++i){
-	myTrade[i].print();
-}
-cout << "open order:\n";
-for (int i = 0; i != openOrder.size() ; ++i){
-	openOrder[i].print();
-}
+	for (int i = 0; i != myTrade.size(); ++i)
+	{
+		myTrade[i].print();
+	}
 
 }
--- test_order.cpp
+++ test_order.cpp
@@ -1,6 +1,7 @@
 #include "order.hpp"
 #include <iostream>
 #include <string>
+#include <cassert>
 using std::string;
 using std::cout;
 int main()
@@ -11,5 +12,6 @@ int quantity = 100;
 double price = 0.7838;
 Order myOrder(name,instrument,quantity,price);
 myOrder.print();
+assert(myOrder.quantity == 100);
 return 0;
 }
